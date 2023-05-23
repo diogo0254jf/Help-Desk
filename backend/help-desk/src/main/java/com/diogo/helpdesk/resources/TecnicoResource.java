1c3670bb875b0cccd07acb493909484fb211e21b
Diogo Costa <46428002+diogo0254jf@users.noreply.github.com>
2023-05-23 15:04:52 -0300

    update criado

--- backend/help-desk/src/main/java/com/diogo/helpdesk/resources/TecnicoResource.java
+++ backend/help-desk/src/main/java/com/diogo/helpdesk/resources/TecnicoResource.java
@@ -2,7 +2,6 @@ package com.diogo.helpdesk.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -14,23 +13,19 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.diogo.helpdesk.domain.Pessoa;
 import com.diogo.helpdesk.domain.Tecnico;
 import com.diogo.helpdesk.domain.dtos.TecnicoDTO;
-import com.diogo.helpdesk.repositories.PessoaRepository;
 import com.diogo.helpdesk.services.TecnicoService;
-import com.diogo.helpdesk.services.exceptions.DataIntegrityViolationException;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
-    @Autowired
-    private PessoaRepository pessoaRepository;
 
     @Autowired
     private TecnicoService service;
@@ -51,25 +46,15 @@ public class TecnicoResource {
     @PostMapping
     public ResponseEntity<Tecnico> create(@Valid @RequestBody TecnicoDTO objDTO) {
         objDTO.setId(null);
-        validaPorCpfEEmail(objDTO);
         Tecnico newObj = service.create(objDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
         return ResponseEntity.created(uri).body(newObj);
     }
 
-    private void validaPorCpfEEmail(TecnicoDTO newObj) {
-        Optional<Pessoa> obj = pessoaRepository.findByCpf(newObj.getCpf());
-
-        if (obj.isPresent() && obj.get().getId() != newObj.getId()) {
-            throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
-        }
-
-        obj = pessoaRepository.findByEmail(newObj.getEmail());
-
-        if (obj.isPresent() && obj.get().getId() != newObj.getId()) {
-            throw new DataIntegrityViolationException("Email já cadastrado no sistema");
-        }
-
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @RequestBody TecnicoDTO objDTO) {
+        Tecnico obj = service.update(id, objDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(new TecnicoDTO(obj));
     }
 
     @DeleteMapping(value = "/{id}")
