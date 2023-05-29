dbb17560a0776880f0d0d2630d89b0877d3c969b
Diogo Costa <46428002+diogo0254jf@users.noreply.github.com>
2023-05-29 11:44:58 -0300

    sistema de notify criado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -31,6 +31,8 @@ import { HomeComponent } from "./components/home/home.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { Toast, ToastrModule } from "ngx-toastr";
+
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent, LoginComponent],
@@ -58,6 +60,13 @@ import { LoginComponent } from './components/login/login.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true,
+      
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
